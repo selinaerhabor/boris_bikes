b8728052fb2cf702767fa83c6395e588dba7ad34
Selina Erhabor <selinaerhabor@gmail.com>
2019-10-29 14:55:14 +0000

    Created spec folder and added spec test file

---
+++ spec/boris_bikes_spec.rb
@@ -0,0 +1,8 @@
+require './lib/boris_bikes.rb'
+
+describe DockingStation do
+  it 'release bike from docking station' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
+end
